57a20c601d9d184815e6147ca7e62a1008641230
Milind <milind@samco.in>
2022-08-10 18:10:35 +0530

    app js changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from "../components/layout/Layout";
+import SSRProvider from "react-bootstrap/SSRProvider";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <SSRProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SSRProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
