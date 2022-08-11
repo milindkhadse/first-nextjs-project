21c1096250e8d148935c5d3445a53a590c2c65c8
Milind <milind@samco.in>
2022-08-11 14:02:31 +0530

    added about and products pages

---
+++ pages/about.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam voluptas
+      nisi corporis sunt accusamus minima ut. Quasi inventore dignissimos
+      delectus deleniti sequi, mollitia illo molestiae distinctio nihil
+      assumenda sint pariatur, dolor esse. Tempora excepturi vel exercitationem
+      ducimus voluptate alias iure accusamus amet quis facilis. Sequi, fugit eos
+      cum quod repudiandae alias aspernatur vero? Facere vitae fuga, incidunt
+      aliquid numquam necessitatibus laborum quia inventore magni, at natus
+      voluptas in deserunt commodi adipisci beatae ipsa repellendus, praesentium
+      labore accusantium delectus. Quos odit voluptate aliquam sapiente,
+      repudiandae reiciendis quas id nam! Aliquam, facilis voluptates incidunt
+      ipsum quod voluptate amet exercitationem dolorem voluptas soluta
+      voluptatibus cumque quis. Culpa dignissimos, harum dolores fugiat at
+      mollitia dolorem ipsa facilis ullam laudantium molestiae inventore
+      excepturi alias porro unde omnis vitae, placeat accusamus velit atque?
+      Reiciendis in, excepturi ullam ipsam nostrum nisi hic pariatur magnam
+      consequuntur unde quasi nobis fugit maxime veniam exercitationem
+      consectetur rem porro quas nihil est? Quam, aut eveniet? Soluta
+      exercitationem corporis, quia itaque a, officiis voluptatem minus
+      voluptates autem quae totam, asperiores similique doloremque est magnam
+      harum consequatur facere neque veritatis laudantium nulla earum!
+      Temporibus officia assumenda omnis aspernatur quae explicabo esse
+      perspiciatis! Nisi distinctio minima maxime iure minus autem, magni ex
+      aperiam placeat veniam tempora earum ullam suscipit beatae accusantium
+      blanditiis est, quasi eaque facilis quibusdam perspiciatis consequatur
+      odit delectus? Esse unde labore, magnam ducimus sapiente corporis aliquid
+      aut impedit minima libero accusantium corrupti recusandae temporibus cum
+      similique quo, rerum veniam suscipit voluptas ex molestiae. Porro nam
+      tempore exercitationem, quaerat autem molestias non dolorem, voluptates
+      incidunt eveniet voluptatum odit vero impedit quae amet doloremque
+      necessitatibus dolores distinctio perspiciatis laborum accusamus velit,
+      voluptate tempora maiores. Recusandae sint ab repudiandae assumenda minima
+      qui error, ipsum eveniet blanditiis accusamus iusto laudantium nesciunt in
+      saepe sunt veniam facere vitae voluptas voluptatem eaque dolor ipsam.
+      Eaque dolorem molestias possimus doloribus in tempora, aspernatur
+      necessitatibus voluptate fugit, maxime repellendus magni quod saepe eum
+      commodi, cum cumque voluptatibus. Expedita doloribus molestiae velit,
+      distinctio debitis eius eum laboriosam? Ullam quas atque, quasi libero
+      inventore officiis enim doloribus ut maxime esse deserunt vero tenetur?
+      Hic eos illum nemo? Libero quae error et.
+    </div>
+  );
+};
+
+export default About;
