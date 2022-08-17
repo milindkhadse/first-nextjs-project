ad448c76305687aaa9ab125aceb0875783e69a35
Milind <milind@samco.in>
2022-08-17 10:29:25 +0530

    icon class removed

--- components/UI/chartIcon.js
+++ components/UI/chartIcon.js
@@ -4,13 +4,13 @@ export const chartIcon = (
     fill="#c2cff9"
     viewBox="0 0 24 24"
     stroke="#335eea"
-    stroke-width="1"
+    strokeWidth="1"
     width={56}
     height={56}
   >
     <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z"
     />
   </svg>
